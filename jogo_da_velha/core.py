730064d4485bd16f2cf3fd9598646c7bb12ddd75
Patrick Antony <patrickantony37159@gmail.com>
2025-01-07 20:09:10 -0300

    add asciiUi module and add imports in core module"

---
+++ jogo_da_velha/core.py
@@ -0,0 +1,3 @@
+from online_words import word as online_word
+from offline_words import words as offline_words
+from tip import genTip
--- jogo_da_velha/online_words.py
+++ jogo_da_velha/online_words.py
@@ -1,17 +1,3 @@
 import requests
 
-secret_word = requests.get('https://api.dicionario-aberto.net/random').json()['word']
-public_word = []
-
-def PublicWordGen(secret_word):
-        if '-' in secret_word:
-            public_word = ['_' if i != '-' else '-' for i in secret_word]
-
-        else:
-            public_word = ['_' if i != ' ' else ' ' for i in secret_word]
-        
-        return ''.join(public_word)
-
-if __name__ == '__main__':
-    public_word = PublicWordGen(secret_word)
-    print(secret_word)
+word = requests.get('https://api.dicionario-aberto.net/random').json()['word']
